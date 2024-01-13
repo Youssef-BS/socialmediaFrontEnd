f51bcec22d9d7917e7b325a0f801b7a648d35137
Djosef0 <youssefbensaid827@gmail.com>
2024-01-13 22:17:03 +0100

    add navbar in component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import "./navbar.css"
 
 const Navbar = () => {
  
     return (
    <>
-   
+  <div className='flex justify-between w-full p-6'>
+    <h1 className='font-semibold text-3xl cursor-pointer'>YsfConnect</h1>
+    <div className='flex'>
+    <p>Home</p>
+    <p>Messanger</p>
+    <p>Friends</p>
+    <p>menu</p>
+    </div>
+  </div> 
    </>
   )
 }
